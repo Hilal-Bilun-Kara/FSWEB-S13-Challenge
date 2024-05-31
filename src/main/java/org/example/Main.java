40cfd1e6ad5ad67345e1aa2251816548e0b37731
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-05-31 13:31:00 +0300

    S13D5 Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.enums.Plan;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Healthplan healthplan = new Healthplan(11,"a okul", Plan.BASIC);
+        Healthplan healthplan1 = new Healthplan(12,"b okul", Plan.MEDIUMS);
+        System.out.println(healthplan1.toString());
+        System.out.println(healthplan.toString());
+
+        String[] healtPlans = new String[3];
+        healtPlans[0] = healthplan.getName();
+        healtPlans[1] = healthplan1.getName();
+        Employee employee = new Employee(1,"Hilal Bilun Kara","hilal@hotmail.com","123456",healtPlans);
+        employee.addHealthplan(2,"c okul");
+        employee.addHealthplan(2,"e okul");
+        employee.addHealthplan(-1,"d okul");
+        employee.addHealthplan(0,"f okul");
+        employee.addHealthplan(8,"g okul");
+        System.out.println(employee.toString());
+
+        System.out.println("*******");
+
+        String[] developerNames = new String[4];
+        Company company = new Company(1,"İclal",100000, developerNames);
+        developerNames[0] = employee.getFullName();
+        company.addEmployee(1,"Gizem ");
+        company.addEmployee(3,"Gökçen ");
+        System.out.println(company.toString());
+        }
 }
